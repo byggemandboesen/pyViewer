fdf4d7a207942e6b6269575d17c49f0bd774fcb0
byggemandboesen <victorboesen@hotmail.com>
2023-07-13 20:03:16 +0200

    :construction: More work on regions

--- src/astropy/region.py
+++ src/astropy/region.py
@@ -3,12 +3,12 @@ import numpy as np
 # from src.astropy.image import ImageChannel
 
 class Region:
-    def __init__(self, img):
+    def __init__(self, shape: tuple):
         '''
         Initialize empty region
         '''
-        self.IMG_SIZE = np.size(img.getChannelImage())
-        self.REGION = np.zeros(self.IMG_SIZE)
+        self.SHAPE = shape
+        self.REGION = np.zeros(shape)
     
     # ------------------------------------------------------------------------------------------------- #
 
@@ -16,33 +16,41 @@ class Region:
         '''
         Reset region back to empty region
         '''
-        self.REGION = np.zeros(self.IMG_SIZE)
+        self.REGION = np.zeros(self.SHAPE)
 
-    def updateImageRegion(self) -> None:
-        '''
-        Updates the region extracted image from current region
-        '''
-        self.IMG_REGION = self.IMG[self.REGION==1]
+    # def updateImageRegion(self) -> None:
+    #     '''
+    #     Updates the region extracted image from current region
+    #     '''
+    #     self.IMG_REGION = self.IMG[self.REGION==1]
 
-    def setBox(self, corner: np.ndarray, width: int, heigt: int) -> None:
+    def setBox(self, corner: np.ndarray, width: int, height: int) -> None:
         '''
         Set region to a box from lower left corner coordinates and widht/heigh of box
-        TODO
         '''
-        self.resetRegion()
-        # Do region stuff
+        end = corner+np.array([width, height])
+        corner[1] = self.SHAPE[1] - corner[1]
+        end[1] = self.SHAPE[1] - end[1]
 
-        self.updateImageRegion()
+        region = np.copy(self.REGION)
+        region[end[1]:corner[1],corner[0]:end[0]] += 1
+
+        return region
     
-    def setEllipse(self, center: np.ndarray, major_ax: int, minor_ax: int) -> None:
+    def setEllipse(self, center: np.ndarray, width: int, height: int, tilt: float) -> None:
         '''
         Set region to an ellipse from center coordinates and major/minor axis.
         If major_ax=minor_ax, a circular region with diameter of the major axis will be applied.
-        TODO
         '''
-        self.resetRegion()
-        # Do region stuff
+        region = np.copy(self.REGION)
+        center[1] = self.SHAPE[1] - center[1]
 
-        self.updateImageRegion()
-
+        a, b = width/2, height/2
+        for i in range(self.SHAPE[0]):
+            for j in range(self.SHAPE[1]):
+                if (j-center[0])**2/a**2+(i-center[1])**2/b**2 < 1:
+                    region[i,j] = 1
+        
+        # TODO - Add tilt angle
+        return region
     # ------------------------------------------------------------------------------------------------- #
--- src/gui/callbacks/region_callbacks.py
+++ src/gui/callbacks/region_callbacks.py
@@ -1,8 +1,77 @@
 import dearpygui.dearpygui as dpg
 import numpy as np
 
+import matplotlib.pyplot as plt
+
+from src.astropy.region import Region
 
 # Add region
 # Update region
 # etc...
 
+
+
+def updateRegionPlot():
+    '''
+    Updates the region plot
+    '''
+    region_type = dpg.get_value("region_type")
+    region_active = dpg.get_value("toggle_region")
+
+    if not region_active:
+        return
+    
+    # Current image shape
+    shape = (int(dpg.get_value("imsize_ra_pix")), int(dpg.get_value("imsize_dec_pix")))
+    if shape == ("", ""):
+        print("No image detected! Not able to generate region")
+        return
+
+    match region_type:
+        case "Ellipse":
+            region = ellipseRegion(shape)
+        case "Box":
+            region = boxRegion(shape)
+        case "Threshold":
+            region = thresholdRegion(shape)
+        case _:
+            return
+    
+    # plt.imshow(region)
+    # plt.show()
+
+    # TODO - Add region to image viewer
+    # TODO - Display region statistics
+
+
+def ellipseRegion(shape: tuple) -> Region:
+    '''
+    Returns an elliptic region instance
+    '''
+    pos = np.array([dpg.get_value("ellipse_x_pos"), dpg.get_value("ellipse_y_pos")])
+    w, h = dpg.get_value("ellipse_width"), dpg.get_value("ellipse_height")
+    tilt = dpg.get_value("ellipse_angle")
+
+    region = Region(shape)
+    ellipse = region.setEllipse(center=pos, width=w, height=h, tilt=tilt)
+    return ellipse
+
+
+def boxRegion(shape: tuple) -> Region:
+    '''
+    Returns a box region instance
+    '''
+    corner = np.array([dpg.get_value("box_x_pos"), dpg.get_value("box_y_pos")])
+    w, h = dpg.get_value("box_width"), dpg.get_value("box_height")
+
+    region = Region(shape)
+    box = region.setBox(corner=corner, width=w, height=h)
+
+    return box
+
+
+def thresholdRegion(shape: tuple) -> Region:
+    '''
+    Returns a threshold region instance
+    '''
+    return
